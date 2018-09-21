fc367d7ed032da51e66ca913c35bc27239ee9dd0
drafting-dreams <543913819@qq.com>
2018-09-21 21:51:05 +0800

    finish the footer

--- src/components/home/Footer.vue
+++ src/components/home/Footer.vue
@@ -1,5 +1,11 @@
 <template>
   <footer>
+    <a target="_blank" href="https://github.com/drafting-dreams">GitHub
+      <svg viewBox="0 0 16 16" version="1.1" aria-hidden="true"><path fill="#1296DB" fill-rule="evenodd" d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.013 8.013 0 0 0 16 8c0-4.42-3.58-8-8-8z"></path></svg>
+    </a>
+    <a target="_blank" href="https://codepen.io/drafting-dreams/">CodePen
+      <img src="../../assets/codepen.png">
+    </a>
     <a target="_blank" href="https://www.youtube.com/channel/UCGc1CjGFmspkt4mGZDtE8Bw">YouTube
       <img src="../../assets/youtube.png">
     </a>
@@ -20,6 +26,11 @@
     a
       font-size: 0
       img
-        width: 50px
-        height: 50px
+        padding: 10px
+        width: 25px
+        height: 25px
+      svg
+        padding: 10px
+        width: 25px
+        height: 25px
 </style>
